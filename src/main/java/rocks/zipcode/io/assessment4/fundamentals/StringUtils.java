8371dfbab0d0432f84f691be5258c3bf9b7665a1
jwilkinson6179 <JamesW6179@gmail.com>
2020-03-30 09:39:36 -0400

    StingUtils done

--- src/main/java/rocks/zipcode/io/assessment4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/io/assessment4/fundamentals/StringUtils.java
@@ -1,22 +1,63 @@
 package rocks.zipcode.io.assessment4.fundamentals;
 
+import java.util.ArrayList;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class StringUtils {
-    public static String capitalizeNthCharacter(String str, Integer indexToCapitalize) {
-        return null;
+    public static String capitalizeNthCharacter(String str, Integer indexToCapitalize)
+    {
+        StringBuilder result = new StringBuilder();
+
+        result.append(str.substring(0, indexToCapitalize));
+        result.append(str.substring(indexToCapitalize, indexToCapitalize + 1).toUpperCase());
+        result.append(str.substring(indexToCapitalize + 1));
+
+        return result.toString();
     }
 
-    public static Boolean isCharacterAtIndex(String baseString, Character characterToCheckFor, Integer indexOfString) {
-        return null;
+    public static Boolean isCharacterAtIndex(String baseString, Character characterToCheckFor, Integer indexOfString)
+    {
+        return (characterToCheckFor.equals(baseString.charAt(indexOfString)));
     }
 
-    public static String[] getAllSubStrings(String string) {
-        return null;
+    public static String[] getAllSubStrings(String string)
+    {
+        ArrayList<String> substrings = new ArrayList<>();
+
+        for(Integer i = 0; i < string.length(); i++)
+        {
+            for(Integer k = i + 1; k <= string.length(); k++)
+            {
+                String currentSub = string.substring(i, k);
+                if(!substrings.contains(currentSub))
+                {
+                    substrings.add(currentSub);
+                }
+            }
+        }
+        String[] array = substrings.toArray(new String[0]);
+
+        return array;
     }
 
-    public static Integer getNumberOfSubStrings(String input){
-        return null;
+    public static Integer getNumberOfSubStrings(String input)
+    {
+        return getAllSubStrings(input).length;
+    }
+
+    public static String[] pushToArrayOfStrings(String addedString, String[] stringOfArrays)
+    {
+        String[] output = new String[stringOfArrays.length + 1];
+
+        for(Integer i = 0; i < stringOfArrays.length; i++)
+        {
+            output[i] = stringOfArrays[i];
+        }
+
+        output[ stringOfArrays.length] = addedString;
+
+        return output;
     }
 }
--- src/main/java/rocks/zipcode/io/assessment4/fundamentals/VowelUtils.java
+++ src/main/java/rocks/zipcode/io/assessment4/fundamentals/VowelUtils.java
@@ -3,21 +3,39 @@ package rocks.zipcode.io.assessment4.fundamentals;
 /**
  * @author leon on 09/12/2018.
  */
-public class VowelUtils {
-    public static Boolean hasVowels(String word) {
-        return null;
+public class VowelUtils
+{
+    public static Boolean hasVowels(String word)
+    {
+        word = word.toLowerCase();
+        return word.contains("a") || word.contains("e") || word.contains("i") || word.contains("o") || word.contains("u");
     }
 
-    public static Integer getIndexOfFirstVowel(String word) {
-        return null;
+
+    public static Integer getIndexOfFirstVowel(String word)
+    {
+        char[] wordAsArray = word.toCharArray();
+
+        for(Integer i = 0; i < wordAsArray.length; i++)
+        {
+            if(isVowel(wordAsArray[i]))
+            {
+                return i;
+            }
+        }
+        return -1;
     }
 
 
-    public static Boolean startsWithVowel(String word) {
-        return null;
+    public static Boolean startsWithVowel(String word)
+    {
+        return isVowel(word.charAt(0));
     }
 
-    public static Boolean isVowel(Character character) {
-        return null;
+    public static Boolean isVowel(Character character)
+    {
+        return character.equals('a') || character.equals('e') || character.equals('i') || character.equals('o') ||
+                character.equals('u') || character.equals('A') || character.equals('E') || character.equals('I') ||
+                character.equals('O') || character.equals('U');
     }
 }
